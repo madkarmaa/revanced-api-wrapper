ecb52cf49d7764fe90ecba309b3c9591a6bdb70d
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-08-10 12:52:49 +0200

    fix: add requirements file to manifest, making build command work

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 import pathlib
-import os
 from setuptools import setup, find_packages
-
-# FIXME: "python -m build" not working
-
-ROOT_DIR = os.path.normpath(os.path.dirname(os.path.abspath(__file__)))
-
-def parse_requirements() -> list[str]:
-    return pathlib.Path(os.path.join(ROOT_DIR, 'requirements.txt')).read_text('utf-8').split('\n')
 
 setup(
     name = 'revanced_api_wrapper',
@@ -15,11 +7,11 @@ setup(
     author = 'MadKarma',
     description = 'Python wrapper for the ReVanced API.',
     long_description_content_type = 'text/markdown',
-    long_description = pathlib.Path(os.path.join(ROOT_DIR, 'README.md')).read_text('utf-8'),
+    long_description = pathlib.Path('README.md').read_text('utf-8'),
     packages = find_packages(),
     include_package_data = True,
     url = 'https://github.com/madkarmaa/revanced-api-wrapper',
     license = 'MIT',
     python_requires = '>=3.10',
-    install_requires = parse_requirements()
+    install_requires = pathlib.Path('requirements.txt').read_text('utf-8').split('\n')
 )
